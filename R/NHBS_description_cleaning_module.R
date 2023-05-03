2fc7eb74ddeabb727b4410a163adef2f56794522
ColinMcKay24 <117296998+ColinMcKay24@users.noreply.github.com>
2023-05-03 10:37:26 -0400

    Update NHBS_description_cleaning_module.R

--- R/NHBS_description_cleaning_module.R
+++ R/NHBS_description_cleaning_module.R
@@ -24,23 +24,4 @@ NHBS_description_cleaner <- function(pest_Desc){
 
 }
 
-view(NHBS_description_cleaner(pest_Desc))
-
-
-
-
-
-
-
-#### Original Code #### 
-# changing column names 
-colnames(pest_Desc) # original column names 
-
-pest_Desc_colnames <- c("pesticide_name", "description", "pesticide_type")
-
-colnames(pest_Desc) <- pest_Desc_colnames
-
-# colnames(pest_Desc) # verify new column names
-
-# eliminating rows with redundant values from transition to csv 
-pest_Desc <- subset(pest_Desc, pest_Desc$pesticide_name != "Pesticide") 
+# view(NHBS_description_cleaner(pest_Desc))
